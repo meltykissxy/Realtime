dc1348ddf79fcef3e28eab5ed351b18a318b9a82
MeltyKiss <xymeltykiss@gmail.com>
2021-01-31 10:22:56 +0800

    新增大段代码

---
+++ Spark/src/main/scala/bean/DauInfo.scala
@@ -0,0 +1,11 @@
+package bean
+
+case class DauInfo(
+                      mid: String,
+                      uid: String,
+                      ar: String,
+                      ch: String,
+                      vc: String,
+                      var dt: String,
+                      var hr: String,
+                      ts: Long)
